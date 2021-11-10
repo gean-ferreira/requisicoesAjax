1ac0a8bb9ecd62db5d897547f789aa99d13b7d98
geanroberto <gean_roberto@hotmail.com>
2021-11-10 20:36:48 -0300

    Requisição com o objeto XMLHttpRequest

---
+++ README.md
@@ -0,0 +1,5 @@
+Esse repositorio mostrará 3 tipos de requisições AJAX utilizando o objeto XMLHttpRequest, Promisse e Async/Await.
+Para praticar será usando a API do github
+
+// API GitHub //
+https://docs.github.com/en/rest/reference/users
--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 var API_URL = "https://api.github.com/users/";
 var USERNAME = "geanroberto";
+
+// Requisição com XMLHttpRequest
+function reqXML() {
+	var httprequest = new XMLHttpRequest();
+	httprequest.onreadystatechange = function () {
+		if (this.readyState == 4 && this.status == 200) {
+			var p = document.getElementById("idReqXML");
+			p.innerHTML = httprequest.responseText;
+		}
+	};
+
+	httprequest.open("get", API_URL + USERNAME, true);
+	httprequest.send();
+}
