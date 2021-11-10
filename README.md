04a1bf6ec7005ec9a75915da1b1d8d437e4afe3e
geanroberto <gean_roberto@hotmail.com>
2021-11-10 20:46:19 -0300

    Requisição com promisse

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 Esse repositorio mostrará 3 tipos de requisições AJAX utilizando o objeto XMLHttpRequest, Promisse e Async/Await.
-Para praticar será usando a API do github
+Para praticar será usando a API do github.
+
+Obs: Apenas tratamento de sucesso nas requisições!
 
 // API GitHub //
 https://docs.github.com/en/rest/reference/users
--- script.js
+++ script.js
@@ -6,11 +6,21 @@ function reqXML() {
 	var httprequest = new XMLHttpRequest();
 	httprequest.onreadystatechange = function () {
 		if (this.readyState == 4 && this.status == 200) {
-			var p = document.getElementById("idReqXML");
+			var p = document.getElementById("ReqXML");
 			p.innerHTML = httprequest.responseText;
 		}
 	};
 
 	httprequest.open("get", API_URL + USERNAME, true);
 	httprequest.send();
+}
+
+// Requisição com Promisse
+function reqPromisse() {
+	fetch(`${API_URL}${USERNAME}`)
+		.then((response) => response.json())
+		.then((dados) => {
+			const p = document.getElementById("ReqPromisse");
+			p.innerHTML = JSON.stringify(dados);
+		})
 }
